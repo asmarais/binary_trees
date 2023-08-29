86a536b1f7e8f124cdcf0cf43b8032aa81ba1749
asmarais <rais.asma99@gmail.com>
2023-08-29 12:25:26 +0100

    binaray tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,10 +9,15 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node;
 
-	if (node == NULL || parent == NULL)
+	if (parent == NULL)
 		return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+
 	node->parent = parent;
 	node->n = value;
 	node->right = NULL;
